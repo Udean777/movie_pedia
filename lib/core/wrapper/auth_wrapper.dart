599e23ef9e6b885dc57a3a4ab874c433b150a5a4
Udean777 <uaart123@gmail.com>
2025-01-30 12:24:03 +0700

    FEAT: Implement main navigation structure with Home, Search, and Wishlist pages

--- lib/core/wrapper/auth_wrapper.dart
+++ lib/core/wrapper/auth_wrapper.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_pedia/core/providers/firebase_auth_provider.dart';
+import 'package:movie_pedia/main_page.dart';
 import 'package:movie_pedia/presentation/auth/signin_page.dart';
-import 'package:movie_pedia/presentation/home/home_page.dart';
 
 class AuthWrapper extends ConsumerWidget {
   const AuthWrapper({super.key});
@@ -12,7 +12,7 @@ class AuthWrapper extends ConsumerWidget {
     final authState = ref.watch(authStateNotifierProvider);
 
     return authState.when(
-      data: (user) => user != null ? const HomePage() : const SigninPage(),
+      data: (user) => user != null ? const MainPage() : const SigninPage(),
       loading: () => const Scaffold(
         body: Center(child: CircularProgressIndicator()),
       ),
---
+++ lib/main_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:movie_pedia/presentation/home/home_page.dart';
+import 'package:movie_pedia/presentation/search/search_page.dart';
+import 'package:movie_pedia/presentation/wishlist/wishlist_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+
+  static final List<Widget> items = <Widget>[
+    HomePage(),
+    SearchPage(),
+    WishlistPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movie Pedia'),
+      ),
+      body: items.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Wishlist',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: colorScheme.primary,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
